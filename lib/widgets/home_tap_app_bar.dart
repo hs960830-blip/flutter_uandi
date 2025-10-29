dee820899c5d0e4b914bb596414680dcda5d0a82
hyeseon <hs960830@gmail.com>
2025-10-29 16:48:29 +0900

    목록 페이지 위젯 폴더 생성

---
+++ lib/widgets/home_tap_app_bar.dart
@@ -0,0 +1,10 @@
+
+
+import 'package:flutter/widgets.dart';
+
+class HomeTapAppBar extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Text('wasabi gimbab records');
+  }
+}
