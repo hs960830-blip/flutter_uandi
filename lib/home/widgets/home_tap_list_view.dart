6f27e635e18be84d3691c6a81bbcd95b51b88733
hyeseon <hs960830@gmail.com>
2025-10-29 21:02:16 +0900

    목록 리스트, appbar 수정

--- lib/home/widgets/home_tap_list_view.dart
+++ lib/home/widgets/home_tap_list_view.dart
@@ -5,6 +5,15 @@ import 'package:flutter/material.dart';
 class HomeTapListView extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return Text('hometaplistview');
+    return Expanded(child: ListView.separated(
+      itemBuilder: (context, index) {
+        return Text('data');
+      }, 
+      separatorBuilder: (context, index) {
+        return Divider(height: 20);
+      }, 
+      itemCount: 5
+      ),
+      );
   }
 }
