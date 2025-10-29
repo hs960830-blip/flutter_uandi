60379121b3ba50a30e76d240de1d096f8fef965a
hyeseon <hs960830@gmail.com>
2025-10-29 11:56:10 +0900

    목록 리스트 생성

--- lib/record_list.dart
+++ lib/record_list.dart
@@ -1,10 +1,16 @@
 
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 
 class RecordList extends StatefulWidget {
   const RecordList({super.key});
+
+  RecordList(this.path, this.title);
+
+  String path;
+  String title;
 
   @override
   State<RecordList> createState() => _RecordListState();
@@ -14,7 +20,38 @@ class _RecordListState extends State<RecordList> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text('wasabi gimbab record',
+        style: TextStyle(
+          fontSize: 15,
+          fontWeight: FontWeight.bold
+        ),),
+        actions: [
+          Icon(CupertinoIcons.cart,
+          size: 30,),
+          SizedBox(width: 20)
+        ],
+      ),
+      body: Column(
+        children: [
+          Row(
+            children: [
+            Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20)) ,
+            Container(
+              width: 100,
+              height: 100,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assete/norahjoneslp.jpg'),
+                  fit: BoxFit.contain),
+              ),
+            )
+           // Image.asset('assets/norahjoneslp.jpg')
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
