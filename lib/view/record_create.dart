f975712beb1bef48bbd1cbb8fa621bf1500412d2
uiiuodo <uiiuodo@gmail.com>
2025-10-29 16:40:50 +0900

    등록페이지

---
+++ lib/view/record_create.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RecordCreate extends StatefulWidget {
+  const RecordCreate({super.key});
+
+  @override
+  State<RecordCreate> createState() => _RecordCreateState();
+}
+
+class _RecordCreateState extends State<RecordCreate> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
